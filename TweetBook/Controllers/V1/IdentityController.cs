12296b8a74fc9465dfe1d87cccbb16ef7140b44b
Ravi Chandra <Ravi Chandra@LAPTOP-B9FOBDT5>
2019-10-08 16:55:00 +0530

    User Registration and Controller Auth

---
+++ TweetBook/Controllers/V1/IdentityController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TweetBook.Contracts.V1;
+using TweetBook.Contracts.V1.Requests;
+using TweetBook.Contracts.V1.Responses;
+using TweetBook.Services;
+
+namespace TweetBook.Controllers.V1
+{
+    public class IdentityController : Controller
+    {
+        private IIdentityService _identityService;
+        public IdentityController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        [HttpPost(ApiRoutes.Identity.Registration)]
+        public async Task<IActionResult> Register([FromBody]UserRegistrationRequest request)
+        {
+            var authResponse = await _identityService.RegisterAsync(request.Email, request.Password);
+
+            if (!authResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResponse.Errors
+                });
+            }
+
+            return Ok(new AuthSuccessResponse
+            {
+                Token = authResponse.Token
+            });
+        }
+    }
+}
